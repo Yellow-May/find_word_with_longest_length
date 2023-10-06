1727af9d073c3aa4fa949c221cd34a7b845ed805
Yellow May <yellow.may.21@gmail.com>
2023-10-06 08:38:10 +0100

    Update

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const vowels = ["a", "e", "i", "o", "u"];
 function action(str) {
   return str
     .split(" ")
-    .map((e) => e.replaceAll(",", "").replaceAll(".", ""))
+    .map((e) =>
+      e
+        .replaceAll(",", "")
+        .replaceAll(".", "")
+        .replaceAll("'", "")
+        .replaceAll("-", "")
+        .replaceAll("_", "")
+    )
     .reduce((a, b) => {
       if (b.length > a.length) {
         return b;
@@ -21,8 +28,18 @@ function action(str) {
     }, "");
 }
 
-console.log(
-  action(
-    "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers"
-  )
-);
+// console.log(
+//   action(
+//     "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers"
+//   )
+// );
+
+// console.log(
+//   action(
+//     "You will be evaluated for both correctness, proper requirement understanding and presentation abilities"
+//   )
+// );
+
+// console.log(action("abilities abilities."));
+
+console.log(action("abilities abilities."));
