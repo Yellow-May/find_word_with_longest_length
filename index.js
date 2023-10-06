b27017d79270560fce686610fd86fce5a3c29a40
Yellow May <yellow.may.21@gmail.com>
2023-10-06 07:51:39 +0100

    Fin

---
+++ index.js
@@ -0,0 +1,28 @@
+const vowels = ["a", "e", "i", "o", "u"];
+
+function action(str) {
+  return str
+    .split(" ")
+    .map((e) => e.replaceAll(",", "").replaceAll(".", ""))
+    .reduce((a, b) => {
+      if (b.length > a.length) {
+        return b;
+      } else if (a.length > b.length) {
+        return a;
+      } else {
+        const a_arr = a.split("").filter((e) => vowels.includes(e));
+        const b_arr = b.split("").filter((e) => vowels.includes(e));
+        if (a_arr.length > b_arr.length) {
+          return a;
+        } else {
+          return b;
+        }
+      }
+    }, "");
+}
+
+console.log(
+  action(
+    "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers"
+  )
+);
